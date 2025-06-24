c9837275b51240183a9a3f40ccf8cb0e707eedfb
DizFlyze999 <daffahazizxpww@gmail.com>
2025-06-25 02:47:30 +0700

    Update fast3.go

--- fast3.go
+++ fast3.go
@@ -68,8 +68,8 @@ func FastSpamRequests(Link string, numRequests int64, concurrency int, timeout t
 	// Jangan di otak atik ini udah pas super fast no komen.
 	transport := &http.Transport{
 		Proxy:               proxyFunc,
-		MaxIdleConns:        65500,
-		MaxIdleConnsPerHost: 65500,
+		MaxIdleConns:        20000,
+		MaxIdleConnsPerHost: 20000,
 		IdleConnTimeout:     2 * time.Second,
 		TLSHandshakeTimeout: 1 * time.Second, //Fast requests
 		DialContext: (&net.Dialer{
@@ -269,7 +269,7 @@ func main() {
 	configPath := flag.String("config", "", "FILE JSON")
 	requestsFlag := flag.Int64("requests", 1000000000, "TOTAL REQUESTS")
 	concurrencyFlag := flag.Int("concurrency", 550, "CONCURRENCY")  //Jangan di lebihkan! 550 Cloudshell & 200 Termux & 750 Vps. Biar di seting sama gua.
-	timeoutFlag := flag.Float64("timeout", 2.7, "WAKTU SETIAP REQUEST") // Jangan di set ulang
+	timeoutFlag := flag.Float64("timeout", 3, "WAKTU SETIAP REQUEST") // Jangan di set ulang
 	methodFlag := flag.String("method", "GET", "HTTP METHOD")
 	logFlag := flag.String("log", "ERROR", "DEBUG, INFO, WARNING, ERROR")
 	noLiveFlag := flag.Bool("no-live", false, "MATIKAN LIVE OUTPUT")
